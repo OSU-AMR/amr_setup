f3b48917049d21b5ff8c3f23131158eae045526e
spyalexs <spyalexs@gmail.com>
2025-07-03 17:57:59 -0400

    Update btc to zenoh

--- colcon_riptide/build_translate_configure.sh
+++ colcon_riptide/build_translate_configure.sh
@@ -4,6 +4,9 @@
 
 cd ~/AMR
 
+#launch a zenoh router
+ros2 run rmw_zenoh_cpp rmw_zenohd &
+
 #colcon build
 colcon build
 
@@ -12,6 +15,9 @@ ros2 run amr_central path_planner.py &
 
 #translate the blueprint
 ros2 run amr_central translate_blueprint.py --ros-args -p blueprint_filename:=map_a.yaml -p command_list_filename:=command_list.yaml
+
+#terminate the zenoh router
+pkill rmw_zenohd
 
 #configure the launcher
 ros2 run amr_launcher configure_for_launch.py
